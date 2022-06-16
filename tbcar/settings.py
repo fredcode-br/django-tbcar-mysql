afd99a334df52d1b8136af71879296d3fe0eeea4
Frederico Rufino <freddiniz57@gmail.com>
2022-06-15 21:57:50 -0300

    make it better

--- tbcar/settings.py
+++ tbcar/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles', BASE_DIR/'staticfiles/img']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
